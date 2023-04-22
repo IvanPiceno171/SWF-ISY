465f2cd206c533522c364d9a0a78610a4e66dd98
stefin <stefinracho@gmail.com>
2023-04-22 12:51:52 -0700

    added map restrictions and data

--- Hackathon/src/GoogleApi.jsx
+++ Hackathon/src/GoogleApi.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useMemo } from 'react'
+import React, { useMemo, useCallback } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 export default function GoogleApi() {
@@ -7,25 +6,52 @@ export default function GoogleApi() {
     googleMapsApiKey: "AIzaSyDLorRCUcHdtNsfZAwF1-yYLhdrIFllPXY",
   });
 
-  if (!isLoaded){
-
+  if (!isLoaded) {
     return <div>Loading...</div>;
-  } else{
-
+  } else {
     return <GoogleMapComponent />;
   }
 }
 
 function GoogleMapComponent() {
-  const center = useMemo(() => ({ lat: 37.325954, lng: -120.499992}), []);
+  const center = useMemo(() => ({ lat: 37.325954, lng: -120.499992 }), []);
+
+  const onMapLoad = useCallback((map) => {
+    const bounds = new window.google.maps.LatLngBounds();
+    const mercedSW = new window.google.maps.LatLng(37.24, -120.5429);
+    const mercedNE = new window.google.maps.LatLng(37.3346, -120.4164);
+
+    bounds.extend(mercedSW);
+    bounds.extend(mercedNE);
+
+    map.fitBounds(bounds);
+  }, []);
+
+  const options = {
+    minZoom: 12,
+    maxZoom: 20,
+    restriction: {
+      latLngBounds: {
+        north: 37.3346,
+        south: 37.24,
+        east: -120.4164,
+        west: -120.5429,
+      },
+      strictBounds: true,
+    },
+  };
 
   return (
-    <GoogleMap zoom={12} center={center} mapContainerClassName="map-container"
-      onClick={(e)=>{
-        console.log("lat", e.latLng.lat())
-        console.log("long", e.latLng.lng())
+    <GoogleMap
+      zoom={12}
+      center={center}
+      options={options}
+      mapContainerClassName="map-container"
+      onClick={(e) => {
+        console.log("lat", e.latLng.lat());
+        console.log("long", e.latLng.lng());
       }}
-    
+      onLoad={onMapLoad}
     >
       <Marker position={center} />
     </GoogleMap>
