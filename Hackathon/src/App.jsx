ba517667d5424be66a5c38f429569658036dd79d
stefin <stefinracho@gmail.com>
2023-04-22 09:57:28 -0700

    google-api-fix

--- Hackathon/src/App.jsx
+++ Hackathon/src/App.jsx
@@ -1,16 +1,13 @@
-import { GoogleApi } from './GoogleApi.jsx'
-import './App.css'
+import GoogleApi from "./GoogleApi.jsx";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
       <h1>Vite + React</h1>
       <GoogleApi></GoogleApi>
-          </>
-
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
